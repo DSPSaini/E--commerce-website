7d2bdfc26935bd37564040eb44c09be0d4a15ef2
DSPSaini <durgasaini9116@gmail.com>
2024-08-24 15:56:04 +0530

    https://github.com/DSPSaini/E--commerce-website.git

---
+++ myapp/src/productData.js
@@ -0,0 +1,87 @@
+
+const Products = [
+    {
+      id: 1,
+      title: "iPhone 10",
+      description: "An apple mobile which is nothing like apple",
+      price: 500,
+      stock: 94,
+      quantity : 1 ,
+      brand: "Apple",
+      category: "smartphones",
+      images: "media/img1.jpg",
+    },
+     {
+       id: 2,
+       quantity : 1,
+       title: "iPhone X",
+       description:
+         "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+       price: 600,
+       stock: 34,
+       brand: "Apple",
+       category: "smartphones",
+       images: "media/img2.jpg",
+     },
+     {
+       id: 3,
+       title: "Samsung Universe 9",
+       quantity : 1 ,
+       description:
+         "Samsung's new variant which goes beyond Galaxy to the Universe",
+       price: 1000,
+       stock: 36,
+       brand: "Samsung",
+       category: "smartphones",
+       images: "media/img3.jpg",
+     },
+     {
+       id: 4,
+       quantity : 1,
+       title: "OPPOF19",
+       description: "OPPO F19 is officially announced on April 2021.",
+       price: 1500,
+       stock: 123,
+       brand: "OPPO", 
+       category: "smartphones",
+       images: "media/img4.jpeg",
+     },
+     {
+       id: 5,
+       title: "Huawei P30",
+       quantity : 1 ,
+       description:
+         "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+       price: 499,
+       stock: 32,
+       brand: "Huawei",
+       category: "smartphones",
+       images: "media/img5.jpg" ,
+     },
+     {
+       id: 6,
+       quantity : 1,
+       title: "MacBook Pro",
+       description:
+         "MacBook Pro 2021 with mini-LED display may launch between September, November",
+       price:2000,
+       stock: 83,
+       brand: "Apple",
+       category: "laptops",
+       images: "media/img6.jpg",
+     },
+     {
+       id: 7,
+       title: "Samsung Galaxy Book",
+       quantity : 1 ,
+       description:
+         "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+       price: 2500,
+       stock: 50,
+       brand: "Samsung",
+       category: "laptops",
+       images: "media/img7.jpg"
+      }
+]
+
+export default Products;
